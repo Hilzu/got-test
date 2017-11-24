434dc9ddac4ce51359b65c93c969fd2eb8f7035a
Ram <ram@hiddentao.com>
2017-11-24 16:41:58 +0000

    more method tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,7 @@
 import got from 'got'
 import Koa from 'koa'
 import KoaRouter from 'koa-router'
+import koaBodyParser from 'koa-bodyparser'
 import { gotServer } from './'
 
 describe('.gotServer()', () => {
@@ -24,7 +25,24 @@ describe('.gotServer()', () => {
       ctx.body = { hello: 'world' }
     })
 
-    app.use(router.routes()).use(router.allowedMethods())
+    router.post('/testpost', function (ctx, next) {
+      ctx.type = 'json'
+      ctx.body = ctx.request.body
+    })
+
+    router.put('/testput', function (ctx, next) {
+      ctx.type = 'json'
+      ctx.body = ctx.request.body
+    })
+
+    router.del('/testdel', function (ctx, next) {
+      ctx.type = 'json'
+      ctx.body = ctx.request.body
+    })
+
+    app
+      .use(koaBodyParser())
+      .use(router.routes())
 
     server = await new Promise(resolve => {
       const s = app.listen(3000, () => resolve(s))
@@ -41,6 +59,7 @@ describe('.gotServer()', () => {
 
       expect(ret.body).toEqual(JSON.stringify({ hello: 'world' }))
       expect(ret.headers['content-type']).toContain('application/json')
+      expect(gotSpy.get).toHaveBeenCalledWith('http://127.0.0.1:3000/test', {})
     })
 
     it('can set options', async () => {
@@ -69,6 +88,156 @@ describe('.gotServer()', () => {
       })
 
       expect(gotSpy.get).toHaveBeenCalledWith('http://127.0.0.1:3000/test', {
+        headers: {
+          'X-Test': 456
+        }
+      })
+    })
+  })
+
+  describe('returns .post() which', () => {
+    it('can make POST requests', async () => {
+      const ret = await gotServer(server).post('/testpost', {
+        json: true,
+        body: { hello: 'world' }
+      })
+
+      expect(ret.body).toEqual({ hello: 'world' })
+      expect(ret.headers['content-type']).toContain('application/json')
+      expect(gotSpy.post).toHaveBeenCalledWith('http://127.0.0.1:3000/testpost', {
+        json: true,
+        body: { hello: 'world' }
+      })
+    })
+
+    it('can set options', async () => {
+      await gotServer(server, {
+        json: true,
+        headers: {
+          'X-Test': 123
+        }
+      }).post('/testpost')
+
+      expect(gotSpy.post).toHaveBeenCalledWith('http://127.0.0.1:3000/testpost', {
+        json: true,
+        headers: {
+          'X-Test': 123
+        }
+      })
+    })
+
+    it('can have options overridden', async () => {
+      await gotServer(server, {
+        headers: {
+          'X-Test': 123
+        }
+      }).post('/testpost', {
+        headers: {
+          'X-Test': 456
+        }
+      })
+
+      expect(gotSpy.post).toHaveBeenCalledWith('http://127.0.0.1:3000/testpost', {
+        headers: {
+          'X-Test': 456
+        }
+      })
+    })
+  })
+
+  describe('returns .put() which', () => {
+    it('can make PUT requests', async () => {
+      const ret = await gotServer(server).put('/testput', {
+        json: true,
+        body: { hello: 'world' }
+      })
+
+      expect(ret.body).toEqual({ hello: 'world' })
+      expect(ret.headers['content-type']).toContain('application/json')
+      expect(gotSpy.put).toHaveBeenCalledWith('http://127.0.0.1:3000/testput', {
+        json: true,
+        body: { hello: 'world' }
+      })
+    })
+
+    it('can set options', async () => {
+      await gotServer(server, {
+        json: true,
+        headers: {
+          'X-Test': 123
+        }
+      }).put('/testput')
+
+      expect(gotSpy.put).toHaveBeenCalledWith('http://127.0.0.1:3000/testput', {
+        json: true,
+        headers: {
+          'X-Test': 123
+        }
+      })
+    })
+
+    it('can have options overridden', async () => {
+      await gotServer(server, {
+        headers: {
+          'X-Test': 123
+        }
+      }).put('/testput', {
+        headers: {
+          'X-Test': 456
+        }
+      })
+
+      expect(gotSpy.put).toHaveBeenCalledWith('http://127.0.0.1:3000/testput', {
+        headers: {
+          'X-Test': 456
+        }
+      })
+    })
+  })
+
+  describe('returns .del() which', () => {
+    it('can make DELETE requests', async () => {
+      const ret = await gotServer(server).del('/testdel', {
+        json: true,
+        body: { hello: 'world' }
+      })
+
+      expect(ret.body).toEqual({ hello: 'world' })
+      expect(ret.headers['content-type']).toContain('application/json')
+      expect(gotSpy.delete).toHaveBeenCalledWith('http://127.0.0.1:3000/testdel', {
+        json: true,
+        body: { hello: 'world' }
+      })
+    })
+
+    it('can set options', async () => {
+      await gotServer(server, {
+        json: true,
+        headers: {
+          'X-Test': 123
+        }
+      }).del('/testdel')
+
+      expect(gotSpy.delete).toHaveBeenCalledWith('http://127.0.0.1:3000/testdel', {
+        json: true,
+        headers: {
+          'X-Test': 123
+        }
+      })
+    })
+
+    it('can have options overridden', async () => {
+      await gotServer(server, {
+        headers: {
+          'X-Test': 123
+        }
+      }).del('/testdel', {
+        headers: {
+          'X-Test': 456
+        }
+      })
+
+      expect(gotSpy.delete).toHaveBeenCalledWith('http://127.0.0.1:3000/testdel', {
         headers: {
           'X-Test': 456
         }
